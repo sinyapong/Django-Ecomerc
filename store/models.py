eed605d08972a5ab9921bfffeb2ed6a10250d31d
yingnaja <sukitogroup@gmail.com>
2024-05-10 14:36:21 +0700

    upload EP8

--- store/models.py
+++ store/models.py
@@ -23,10 +23,13 @@ class Customer(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=8)
     Category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     description = models.CharField(max_length=250,default='',blank=True,null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Add Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=8)
 
     def __str__(self):
         return self.name
